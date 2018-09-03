df7c231d755cbe87d83f21fbc8cd8175ec007d94
Shashank <shashanksinghsolanki@muj.manipal.edu>
2018-09-03 23:46:00 +0530

    Changed Map Style

--- client/src/app/map-box/map-box.component.ts
+++ client/src/app/map-box/map-box.component.ts
@@ -4,7 +4,7 @@ import { MapService } from '../map.service';
 import { GeoJson, FeatureCollection } from '../map';
 
 @Component({
-  selector: 'map-box',
+  selector: 'app-map-box',
   templateUrl: './map-box.component.html',
   styleUrls: ['./map-box.component.scss'],
   providers: [MapService]
@@ -12,28 +12,26 @@ import { GeoJson, FeatureCollection } from '../map';
 export class MapBoxComponent implements OnInit {
   // settings
   map: mapboxgl.Map;
-  style: 'mapbox://styles/kriss1897/cjlm7ias536vx2ss944qunq0a';
-  lat:number = -74.50;
-  lng:number = 40;
+  style = 'mapbox://styles/kriss1897/cjlm7ias536vx2ss944qunq0a';
+  lat = -74.50;
+  lng = 40;
   zoom: 4;
-  message = "Hello World!";
+  message = 'Hello World!';
 
   // data
   source: any;
   markers: any;
 
   // constructor
-  constructor(private mapService: MapService)
-  {
-  }
+  constructor(private mapService: MapService) { }
 
   ngOnInit() {
-    this.markers = []
+    this.markers = [];
     this.initializeMap();
   }
 
   // initialize map
-  private initializeMap(){
+  private initializeMap() {
     /// locate the user
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
@@ -41,27 +39,27 @@ export class MapBoxComponent implements OnInit {
         this.lng = position.coords.longitude;
         this.map.flyTo({
           center: [this.lng, this.lat]
-        })
+        });
       });
     }
 
-   this.buildMap()
+   this.buildMap();
   }
 
-  buildMap(){    
+  buildMap() {
     this.map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://sprites/mapbox/dark-v9',
-      center: [this.lat,this.lng],
+      style: this.style,
+      center: [this.lat, this.lng],
       zoom: 3
-    })
+    });
 
     this.map.addControl(new mapboxgl.NavigationControl());
   }
 
-  flyTo(data: GeoJson){
+  flyTo(data: GeoJson) {
     this.map.flyTo({
       center: data.geometry.coordinates
-    })
+    });
   }
 }
